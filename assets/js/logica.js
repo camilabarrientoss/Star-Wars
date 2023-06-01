d8c5120bacc229e1dd90c95aa4c7d87e8ee5668f
Camila Barrientos <camilabarrientos79@gmail.com>
2023-05-31 22:30:48 -0400

    Segundo commit

--- assets/js/logica.js
+++ assets/js/logica.js
@@ -1,57 +1,52 @@
 const URL_BASE = 'https://swapi.dev/api';
-const URL_PEOPLE1 = URL_BASE + 'people/1';
-const URL_PEOPLE2 = URL_BASE + 'people/2';
-const URL_PEOPLE3 = URL_BASE + 'people/3';
-const URL_PEOPLE4 = URL_BASE + 'people/4';
-const URL_PEOPLE5 = URL_BASE + 'people/5';
+const populares = document.getElementById('card1');
+const infoPopulares = document.getElementById('info1');
+const secundarios = document.getElementById('card2');
+const infoSecundarios = document.getElementById('info2');
+const significativos = document.getElementById('card3');
+const infoSignificativos = document.getElementById('info3');
 
-let carta, boton, datos, notGo = false, gen;
+//OBTENCION DE DATOS
+const request = async id => {
+  const data = await fetch(URL_BASE + `/people/${id}`);
+  const result = await data.json();
+  return result;
+}
 
-function *tarjetaGenerador(datos) {
-    carta.innerHTML = '';
-    for (let temp of datos) {
-        yield temp.name;
-        carta.innerHTML += ` 
-              <div id="tarjSola" class="card mb-3 container" style="max-width: 540px;">
-        <div class="row g-0">
-          <div class="col-md-4">
-            <img src="${temp.image}" class="img-fluid rounded-start" alt="...">
-          </div>
-          <div class="col-md-8">
-            <div class="card-body">
-              <h5 class="card-title">"NOMBRE: ${temp.name}"</h5>
-              <p class="card-text">ESPECIE: "${temp.species}"</p>
-             </div>
-          </div>
-        </div>
+//DEFINICION DE FUNCION GENERADORA
+async function* getData(start, finish) {
+  let id = start;
+  do {
+    const people = await request(id);
+    yield people;
+    id++
+  } while (id <= finish);
+}
+
+//EJECUCION DE FUNCIONES GENERADORAS SEGUN RANGO
+const popularesData = getData(1, 5);
+const secundariosData = getData(6, 11);
+const significativosData = getData(12, 17);
+
+//SALIDA DE INFORMACION
+const cardAdded = async (genFunction, tagHtml, classCirculo) => {
+  let { value } = await genFunction.next();
+  if (value) {
+    return tagHtml.innerHTML += `
+
+    <div class="card">
+      <div class="card-body">
+      <p class="${classCirculo}"></p>
+        <h5 class="card-title text-capitalize">${value.name}</h5>
+        <p class="card-text">Estatura: ${value.height} cm. Peso: ${value.mass} kg.</p>
       </div>
-         `;
-         
-    }
+    </div>
+    </div>
+    `
+  }
 }
 
-const revelaPersonaje = (event) => {
-    event.preventDefault();
-    if (!notGo) {
-        const { value, done } = gen.next();
-        console.log('value:', value);
-        console.log('done:', done);
-        notGo = done;
-    } else {
-        console.log('Nada más que mostrar');
-        datos = undefined;
-        boton.style.display = "none";
-    }
-}
-
-document.addEventListener('DOMContentLoaded', async () => {
-    carta = document.getElementById('carta');
-    boton = document.getElementById('boton');
-    const results = await fetch(URL_CHARACTERS);
-    const response = await results.json();
-    datos = response.results;
-    console.log('datos:', datos);
-    gen = tarjetaGenerador(datos);
-    gen.next();
-    boton.addEventListener('mouseover', revelaPersonaje);
-});
+// INTERACCION USUARIO
+populares.addEventListener('mouseover', () => cardAdded(popularesData, infoPopulares, 'circulo1'));
+secundarios.addEventListener('mouseover', () => cardAdded(secundariosData, infoSecundarios, 'circulo2'));
+significativos.addEventListener('mouseover', () => cardAdded(significativosData, infoSignificativos, 'circulo3'));
--- readme.txt
+++ readme.txt
@@ -3,5 +3,3 @@ https://github.com/camilabarrientoss/Star-Wars
 
 Para clonar: 
 git clone https://github.com/camilabarrientoss/Star-Wars.git
-
-Aun esta en preceso!! :(
